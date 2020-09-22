90de1f284294360058808d50bfbc015b4f4ca96c
Gajendra Tyagi <gajendratyagi1991@gmail.com>
2020-09-22 19:05:00 +0530

    end of section 5

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -14,17 +14,17 @@ export class NavComponent implements OnInit {
   ngOnInit() {
   }
 
-  login(){
+  login() {
     this.authService.login(this.model).subscribe(
       next =>{
         console.log('Logged in successfully');
       }, error => {
-        console.log('Failed to login');
+        console.log(error);
       }
     )
   }
 
-  loggedIn(){
+  loggedIn() {
     const token = localStorage.getItem('token');
     return !!token;
   }
